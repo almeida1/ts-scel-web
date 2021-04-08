2c54a185170e4c65881ac9c7a1039d71b3252d85
Edson Saraiva de Almeida <68782201+ffvprogweb@users.noreply.github.com>
2021-04-08 10:00:51 -0300

    Refatoração do logout - nao invalidava a sessao

--- scel/src/main/java/com/fatec/scel/controller/HomeController.java
+++ scel/src/main/java/com/fatec/scel/controller/HomeController.java
@@ -1,19 +1,39 @@
 package com.fatec.scel.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	@GetMapping("/login")
 	public ModelAndView autenticacao() {
+		     
 		return new ModelAndView("paginaLogin");
 	}
 
 	@GetMapping("/")
 	public ModelAndView home() {
 		return new ModelAndView ("paginaMenu");
+	}
+	@GetMapping("/logout")
+	public ModelAndView logout(HttpServletRequest request, Authentication authentication) {
+		logger.info("Logout para o usuario: " + authentication.getName());
+		HttpSession session= request.getSession(false);
+        SecurityContextHolder.clearContext();
+        if(session != null) {
+            session.invalidate();
+        }
+
+        return new ModelAndView("paginaLogin");
 	}
 		
 }
--- scel/src/main/java/com/fatec/scel/security/WebSecurityConfig.java
+++ scel/src/main/java/com/fatec/scel/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.fatec.scel.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,6 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	CustomizeLogoutSuccessHandler customizeLogoutSuccessHandler;
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
@@ -17,8 +20,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers("/livros/cadastrar").hasRole("BIB") // somente login maria
 			.anyRequest().authenticated()
 			.and()
-			.formLogin().loginPage("/login").permitAll().and().logout()
-			.logoutUrl("/login?logout").permitAll();
+			.formLogin().loginPage("/login").permitAll()
+			.defaultSuccessUrl("/", true)
+			.and()
+			// sample logout customization
+				.logout().deleteCookies("remove").invalidateHttpSession(false)
+				.logoutUrl("/custom-logout").logoutSuccessUrl("/logout-success");
+
+		
+		
 	}
 
 	@Override
